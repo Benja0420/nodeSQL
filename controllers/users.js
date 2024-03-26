32cc6cd50106932a906729662b4fc2af24162d61
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2024-03-26 16:51:31 -0300

    read and create users

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,36 @@
 import { pool } from "../config/database.js";
 
+
+
+//get all users
+
 export const getUsers = async (req, res) => {
     try {
-        const [result] = await pool.query('SELECT * FROM users LIMIT 2');
+        const [result] = await pool.query('SELECT * FROM users');
         res.json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+//get user by id
+
+export const getUserById = async (req, res) => {
+    try {
+        const [result] = await pool.query('SELECT * FROM users WHERE id = ?', [req.params.id]);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+//create user
+
+export const createUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const [result] = await pool.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password]);
+        res.status(201).json({ id: result.insertId, name, email, password });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { getUsers } from '../controllers/users.js';
+import { getUsers, getUserById, createUser } from '../controllers/users.js';
 
 
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
 
 export default router;
