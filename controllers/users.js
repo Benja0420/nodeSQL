5465e3726f36f81d36df5a667dc9c666bddc57c9
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2024-03-26 13:37:11 -0300

    Read users from database

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ import { pool } from "../config/database.js";
 
 export const getUsers = async (req, res) => {
     try {
-        const [result] = await pool.query('SELECT "hola mundo!" AS result');
+        const [result] = await pool.query('SELECT * FROM users LIMIT 2');
         res.json(result);
     } catch (error) {
         res.status(500).json({ message: error.message });
